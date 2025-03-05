8b24105d57420ac99df36181fdffb9c9856695aa
pnietohurtado <pnietohurtado03@gmail.com>
2025-03-05 20:13:04 +0100

    Prueba

--- 05.03.2025/PracticasPOO1/src/main/java/com/mycompany/practicaspoo1/PracticasPOO1.java
+++ 05.03.2025/PracticasPOO1/src/main/java/com/mycompany/practicaspoo1/PracticasPOO1.java
@@ -11,6 +11,6 @@ package com.mycompany.practicaspoo1;
 public class PracticasPOO1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo"); 
     }
 }
